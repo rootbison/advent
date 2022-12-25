9e5240f086249d2d1b8a4cd2969df61346363861
rb <rootbison@gmail.com>
2022-12-24 17:16:19 -0800

    problem 1 idiomatic

--- main/kotlin/problem-1-idiomatic.kt
+++ main/kotlin/problem-1-idiomatic.kt
@@ -3,7 +3,7 @@ import java.io.File
 const val NEWLINE_WINDOWS: String = "\r\n\r\n"
 fun main(args: Array<String>) {
     val input: String = File("in/problem-1.txt").readText()
-    val data: List<Int> = input.split(NEWLINE_WINDOWS).map { it: String -> it.lines().sumOf { it.toInt() } }
-    val max: Int = data.max()
+    val data: List<List<Int>> = input.split(NEWLINE_WINDOWS).map { it: String -> it.lines().map { it.toInt() } }
+    val max: Int = data.maxOf { it.sum()}
     println(max)
 }
