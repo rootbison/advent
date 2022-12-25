e9e0fd3fd730e01d67a0df66867bcb03ac474f47
rb <rootbison@gmail.com>
2022-12-24 17:35:56 -0800

    problem 1 idiomatic

--- main/kotlin/problem-1-idiomatic.kt
+++ main/kotlin/problem-1-idiomatic.kt
@@ -1,9 +1,26 @@
 import java.io.File
 
 const val NEWLINE_WINDOWS: String = "\r\n\r\n"
+
 fun main(args: Array<String>) {
-    val input: String = File("in/problem-1.txt").readText()
-    val data: List<List<Int>> = input.split(NEWLINE_WINDOWS).map { it: String -> it.lines().map { it.toInt() } }
-    val max: Int = data.maxOf { it.sum()}
-    println(max)
+
+    fun parseInput(): List<List<Int>> {
+        val text = File("in/problem-1.txt").readText()
+        return text.split(NEWLINE_WINDOWS).map { it: String -> it.lines().map { it.toInt() } }
+    }
+
+    fun part1(): Int {
+        return parseInput().maxOf { it.sum() }
+    }
+
+    fun part2(): Int {
+        val sums = parseInput().map { it.sum() }
+        return sums.sorted().takeLast(3).sum()
+    }
+
+    println(part1())
+    println(part2())
+
 }
+
+
