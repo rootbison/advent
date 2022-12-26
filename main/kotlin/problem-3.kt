62d280baf5a1e1f0ac4ebd87ee995e0d03485440
rb <rootbison@gmail.com>
2022-12-26 12:47:44 -0800

    problem 3 complete idiomatic

--- main/kotlin/problem-3.kt
+++ main/kotlin/problem-3.kt
@@ -1,41 +1,47 @@
 import java.io.File
 
 data class Rucksack(val pouch1: String, val pouch2: String) {
-    private val duplicate: Char = pouch1.toSet().intersect(pouch2.toSet()).first()
-
-    fun getPriority(): Int {
-        // uppercase: 64 - 26
-        return if (duplicate.isUpperCase()) duplicate.code - 38
-        else duplicate.code - 96
-    }
+    val duplicate: Char = pouch1.toSet().intersect(pouch2.toSet()).first()
+    val contents: String =  pouch1 + pouch2
 }
-data class RucksackGroup(val pouch1: String, val pouch2: String) {
-    private val duplicate: Char = pouch1.toSet().intersect(pouch2.toSet()).first()
-
-    fun getPriority(): Int {
-        // uppercase: 64 - 26
-        return if (duplicate.isUpperCase()) duplicate.code - 38
-        else duplicate.code - 96
-    }
+data class RucksackGroup(val rucksacks: List<Rucksack>) {
+    val duplicate: Char =  rucksacks
+        .map { it.contents.toSet() }
+        .reduce {acc, next -> acc.intersect(next)}
+        .first()
 }
 
 fun main(args: Array<String>) {
     println("Program arguments: ${args.joinToString()}")
 
-    fun parseInput(): String {
-        return File("in/problem-3.txt").readText()
+    fun getPriority(char: Char): Int {
+        // uppercase: 64 - 26
+        return if (char.isUpperCase()) char.code - 38
+        else char.code - 96
+    }
+
+    fun parseInput(): List<Rucksack> {
+        val text = File("in/problem-3.txt").readText()
+        return text.split(NEWLINE_WINDOWS)
+            .map { Rucksack(it.substring(0, it.length / 2), it.substring(it.length / 2)) }
     }
 
     fun part1(): Int {
-        val rucksacks: List<Rucksack> =  parseInput().split(NEWLINE_WINDOWS)
-            .map { Rucksack(it.substring(0, it.length / 2), it.substring(it.length / 2)) }
+        val rucksacks = parseInput()
         return rucksacks
-            .map { elem: Rucksack -> elem.getPriority() }
+            .map { rucksack: Rucksack -> getPriority(rucksack.duplicate) }
             .reduce {acc, score -> acc + score}
     }
 
+    fun part2(): Int {
+        val rucksackGroups = parseInput().chunked(3).map { RucksackGroup(it) }
+        return rucksackGroups
+            .map { rucksackGroup: RucksackGroup -> getPriority(rucksackGroup.duplicate) }
+            .reduce {acc, score -> acc + score}
+    }
 
     println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
 
 }
 
